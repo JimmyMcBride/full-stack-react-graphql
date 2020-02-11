a7c40b90deb3e130ecc2fa0e1540eded950b3d41
JimmyMcBride <mcbride967@gmail.com>
2020-02-11 16:25:43 -0600

    nested data update

--- api/src/index.js
+++ api/src/index.js
@@ -6,6 +6,19 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     hello: String!
+    users: [User!]!
+    posts: [Post!]!
+  }
+  type User {
+    id: ID!
+    username: String!
+    posts: [Post!]
+  }
+  type Post {
+    id: ID!
+    # author: User!
+    title: String!
+    body: String!
   }
 `;
 
@@ -13,8 +26,66 @@ const typeDefs = gql`
 that we defined in our typeDefs */
 const resolvers = {
   Query: {
-    hello: () => "Hello, world!"
+    hello: () => "Hello, world!",
+    users: () => [
+      {
+        id: 1,
+        username: "FireNinja",
+        posts: [
+          {
+            id: 1,
+            // author: 1,
+            title: "Title 1",
+            body: "This is my body, don't touch it!"
+          },
+          {
+            id: 2,
+            // author: 1,
+            title: "Title 2",
+            body: "This is my body, don't touch it!"
+          },
+          {
+            id: 3,
+            // author: 1,
+            title: "Title 3",
+            body: "This is my body, don't touch it!"
+          }
+        ]
+      },
+      {
+        id: 2,
+        username: "BillyTheKid"
+      },
+      {
+        id: 3,
+        username: "GeorgieBoi"
+      }
+    ],
+    posts: () => [
+      {
+        id: 1,
+        author: 1,
+        title: "Title 1",
+        body: "This is my body, don't touch it!"
+      },
+      {
+        id: 2,
+        author: 1,
+        title: "Title 2",
+        body: "This is my body, don't touch it!"
+      },
+      {
+        id: 3,
+        author: 1,
+        title: "Title 3",
+        body: "This is my body, don't touch it!"
+      }
+    ]
   }
+  // Users: {
+  //   id: () => 1,
+  //   username: () => "FireNinja"
+  // }
 };
 
 // Now we need to create an instance of our Apollo server
